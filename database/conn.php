437c06daa223fbd6bdb5d677bea84fd92250bd29
codingidol <82130669+codingidol@users.noreply.github.com>
2021-04-08 10:36:33 +0530

    update

--- database/conn.php
+++ database/conn.php
@@ -1,4 +1,5 @@
 <?php
 //database connection
-($GLOBALS["___mysqli_ston"] = mysqli_connect("localhost","root","","blog_admin_db"));  //host,user,password,database
+($GLOBALS["___mysqli_ston"] = mysqli_connect("freedb.techt","
+freedbtech_mpcodingclub","Coding@1#2","freedbtech_blogadmindb"));  //host,user,password,database
 ?>
