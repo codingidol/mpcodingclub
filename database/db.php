539857f00deee98260010386c86d4354a7a95e6b
codingidol <82130669+codingidol@users.noreply.github.com>
2021-04-08 10:40:38 +0530

    update

--- database/db.php
+++ database/db.php
@@ -1,5 +1,5 @@
 <?php
-	$database_username = 'root';//user name
-	$database_password = '';//password
-	$pdo_conn = new PDO( 'mysql:host=freedb.tech;dbname=freedbtech_blogadmindb', $freedbtech_mpcodingclub, $Coding@1#2 );
+	$database_username = 'freedbtech_mpcodingclub';//user name
+	$database_password = 'Coding@1#2';//password
+	$pdo_conn = new PDO( 'mysql:host=freedb.tech;dbname=freedbtech_blogadmindb', $database_username, $database_password );
 ?>
